33e430bdb34269ce970dbaff7949dc4c7b7d4b2f
1804449@kiit.ac.in <1804449@kiit.ac.in>
2020-12-23 23:08:05 +0530

    Server Establishment and Client Side

---
+++ src/Tcp/Client.java
@@ -0,0 +1,27 @@
+package Tcp;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public Client() throws IOException{
+        Socket socket=new Socket("localhost",2020);
+        System.out.println("Successful Connection Established to the Server.");
+
+        BufferedReader in_socket=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out_socket=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        try {
+            new Client();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
