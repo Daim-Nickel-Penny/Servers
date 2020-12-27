7b2da4f2961d64c7ae1be261496eef0bc9b04656
1804449@kiit.ac.in <1804449@kiit.ac.in>
2020-12-27 20:05:45 +0530

    Java Socket Multithread Advanced.

--- src/Multithread_Tcp/ServerThread.java
+++ src/Multithread_Tcp/ServerThread.java
@@ -9,23 +9,30 @@ import java.net.Socket;
 public class ServerThread implements Runnable {
 
     private Socket socket;
-    public ServerThread (Socket socket){
+    private ServerMain server_main;
+    public ServerThread (Socket socket,
+                         ServerMain server_main){
         this.socket=socket;
+        this.server_main=server_main;
     }
 
     @Override
     public void run(){
         try {
-            System.out.println("Client has connected");
+
+            int client_number=server_main.getClientNumber();
+
+
+            System.out.println("Client "+client_number+" has connected");
             BufferedReader in_socket=new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out_socket=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
 
-            out_socket.println("Welcome");
+            out_socket.println("Welcome. Your Client id is:#"+client_number+" . Whats your message?");
             String message=in_socket.readLine();
             System.out.println("Client Says: "+message);
 
             socket.close();
-            System.out.println("Socket is closed.");
+            System.out.println("Client"+client_number+" has disconnected.");
 
 
         }catch (Exception e){
